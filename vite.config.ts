2524a7235dce3852d9e6b46d934fa1c1bfb4e007
김기태 <kku98057@naver.com>
2024-10-05 16:26:56 +0900

    fix.viteconfig의 base변경

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myBook/',
   css: {
     postcss: {
       plugins: [tailwindcss()],
